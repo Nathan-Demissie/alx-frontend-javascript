afe4a7ee21975203c7bebe5ec05509e26d2fd66a
Avstel <it.nathan.demissie@gmail.com>
2025-09-14 15:55:23 +0300

    Update main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,11 +1,10 @@
-// Define the Teacher interface
 interface Teacher {
   readonly firstName: string;
   readonly lastName: string;
   fullTimeEmployee: boolean;
-  yearsOfExperience?: number;
   location: string;
-  [key: string]: any; // Allows additional properties like 'contract'
+  yearsOfExperience?: number;
+  [key: string]: any;
 }
 
 // Example usage
