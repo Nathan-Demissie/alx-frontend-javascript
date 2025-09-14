b0e04a8be0c96f44736161f9c6b5060d7157f20c
Avstel <it.nathan.demissie@gmail.com>
2025-09-14 16:23:06 +0300

    Update main.ts

--- task_1/js/main.ts
+++ task_1/js/main.ts
@@ -25,8 +25,9 @@ interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
-const printTeacher: printTeacherFunction = (firstName, lastName) => {
+function printTeacher({ firstName, lastName }: { firstName: string; lastName: string }): string {
   return `${firstName.charAt(0)}. ${lastName}`;
-};
+}
 
-console.log(printTeacher("John", "Doe")); // Output: J. Doe
+console.log(printTeacher({ firstName: "John", lastName: "Doe" }));
+
